abefa6aaf2666b3e0ca720113cc7d12d92bac3f6
esycheva <sychevaea@gmail.com>
2024-09-09 09:47:28 +0300

    Add by_time logic

--- app/models/temperature_record.rb
+++ app/models/temperature_record.rb
@@ -22,6 +22,39 @@ class TemperatureRecord < ApplicationRecord
         day.average('value')
     end
 
-    def self.by_time
+    def self.by_time(seconds)
+        min_distance_id = self.min_distance_id(seconds)
+        TemperatureRecord.find(min_distance_id)
+    end
+
+    def self.min_distance(seconds)
+        str = "SELECT MIN(ABS(epoch_time - #{seconds})) as min_distance FROM temperature_records"
+        results = connection.select_all(str)
+
+        if results.present?
+            return results[0]['min_distance']
+        else
+            return nil
+        end
+    end
+
+    def self.min_distance_id(seconds, min)
+        dt = Time.at(seconds)
+        sql = <<-SQL
+            SELECT id,
+                   CASE WHEN ABS(epoch_time - #{seconds}) = #{min} THEN 1
+                   ELSE 0
+                   END
+            FROM temperature_records
+            WHERE temperature_records.local_observation_date_time
+                  BETWEEN '#{dt - 24.hours}' AND '#{dt + 24.hours}';
+        SQL
+        results = connection.select_all(sql)
+
+        if results.present?
+            return results.detect{|i| i['case'] == 1}['id']
+        else
+            return nil
+        end
     end
 end
